5e8a5eb5a25c349c778cb53ae1656995bc935b1d
Rafael Paolini <rafapaolini@gmail.com>
2025-08-13 23:01:48 -0300

    Adiciona arquivo .bat revisado para execução correta

--- executar_conversor.bat
+++ executar_conversor.bat
@@ -1,12 +1,13 @@
 @echo off
+REM Define UTF-8 no console
 chcp 65001 >nul
 title Conversor de Moedas - Programa ONE
 
-REM Pasta raiz do projeto
-set ROOT_DIR=%~dp0
+REM Pasta raiz do projeto (com aspas para caminhos com espaços)
+set "ROOT_DIR=%~dp0"
 
 REM Pasta onde os .class compilados vão ficar
-set OUT_DIR=%ROOT_DIR%bin
+set "OUT_DIR=%ROOT_DIR%bin"
 
 REM Cria a pasta bin se não existir
 if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
